08ae5f85222c5834b1f1105f14d2c1c0c7a47a3b
Aakash Tiwari <0173cs211001@gmail.com>
2026-05-28 15:40:26 +0530

    connect atlas mangodb with python in local and perform CRUD operation on small project

---
+++ 12_python_mongodb/youtube_manager_mongodb.py
@@ -0,0 +1,6 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://Youtubemanagepy:Youtubemanagepy@youyubemanage.hly11ev.mongodb.net/youtubemanage")
+
+
+# not a good idea to include id and password on code files
